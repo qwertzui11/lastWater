7cea452ab37497647d360af242c41ce56cf2332c
Markus Lanner <github@blub-game.com>
2010-12-08 14:26:38 +0100

    several classes init

--- app/game/main.cpp
+++ app/game/main.cpp
@@ -1,30 +1,11 @@
-#include <SFML/Graphics.hpp>
+#include "game.hpp"
 
 
 int main(int argc, char *argv[])
 {
-    // Create the main rendering window
-    sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Graphics");
+    game gm;
+    gm.initialise();
 
-    // Start game loop
-    while (App.IsOpened())
-    {
-        // Process events
-        sf::Event Event;
-        while (App.GetEvent(Event))
-        {
-            // Close window : exit
-            if (Event.Type == sf::Event::Closed)
-                App.Close();
-        }
-
-        // Clear the screen (fill it with black color)
-        App.Clear();
-
-        // Display window contents on screen
-        App.Display();
-    }
-
-    return EXIT_SUCCESS;
+    return gm.run();
 
 }
