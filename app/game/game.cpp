92162d15f794e3266e1a94d7a03cad8aefc6b85e
Markus Lanner <github@blub-game.com>
2010-12-17 18:36:19 +0100

    some work done

--- app/game/game.cpp
+++ app/game/game.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 game::game()
-    : m_window(sf::VideoMode(1024, 768, 32), "lastWater")
+    : m_window(sf::VideoMode(1600, 1200, 32), "lastWater")
     , m_world(&m_window)
     , m_nextAsteroid(0.f)
 {
