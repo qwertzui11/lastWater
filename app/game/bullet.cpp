402bb5cf1cd0cbb2ee89713428243032aa9b3f39
Markus Lanner <markus@blub-game.com>
2011-01-16 19:23:11 +0100

    sounds r implemented volume 9

--- app/game/bullet.cpp
+++ app/game/bullet.cpp
@@ -20,7 +20,7 @@ bullet::bullet(sf::Vector2f pos, sf::Vector2f dir, sf::Image *img, sf::RenderWin
 
     m_sprite.SetRotation(res);
 
-    m_sndBullet.SetVolume(20.f);
+    m_sndBullet.SetVolume(10.f);
     if (m_rw->GetDefaultView().GetRect().Contains(pos.x, pos.y))
         m_sndBullet.Play();
 }
